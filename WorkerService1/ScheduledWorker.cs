c96626b8afcf200b9a546f3e72e76cae481a7874
Alexoandros <alexoandros@outlook.com>
2024-05-09 12:49:01 +0200

    Cambios ejecucion Scheduled

--- WorkerService1/ScheduledWorker.cs
+++ WorkerService1/ScheduledWorker.cs
@@ -16,33 +16,26 @@ internal class ScheduledWorker : BackgroundService
 
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
-        _timer = new Timer(DoWork, null, TimeSpan.Zero, TimeSpan.FromDays(1));
-        await Task.CompletedTask;
-    }
-
-
-    private void DoWork(object? state)
-    {
         var timeEx = _configuration.GetSection("TimeEx").Get<TimeEx>();
         var now = DateTime.Now;
         var target =
             new DateTime(now.Year, now.Month, now.Day, timeEx.Hora, timeEx.Minuto,
-                timeEx.Segundo); // Set to 23:59:59 today
-        if (now < target)
-        {
-            _logger.LogInformation("Executing scheduled work.");
-            _start.StartMethod();
+                timeEx.Segundo); 
+        if (now > target)
+        {            
+            target = target.AddDays(1);
         }
-        else
-        {
-            // Calculate the next run time
-            var nextRun = target.AddDays(1);
-            var waitTime = nextRun - now;
-            // Reschedule the timer
-            _timer?.Change(waitTime, TimeSpan.FromDays(1));
-        }
+
+        var delay = target - now;
+        _timer = new Timer(DoWork, null, delay, TimeSpan.FromDays(1));
+        await Task.CompletedTask;
     }
 
+    private void DoWork(object? state)
+    {
+        _logger.LogInformation("Executing scheduled work.");
+        _start.StartMethod();
+    }
 
     public override async Task StopAsync(CancellationToken stoppingToken)
     {
@@ -50,7 +43,6 @@ internal class ScheduledWorker : BackgroundService
         _timer?.Change(Timeout.Infinite, 0);
         await base.StopAsync(stoppingToken);
     }
-
 
     public override void Dispose()
     {
